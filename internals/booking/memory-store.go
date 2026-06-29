43295b15296e41031d1cd2d75c67690e47d56903
youness oukhadjou <brunnotools@gmail.com>
2026-06-29 10:28:24 +0100

    added in-memory data holder

---
+++ internals/booking/memory-store.go
@@ -0,0 +1,15 @@
+package booking 
+
+
+type MemoryStore struct {
+	bookings map[string]Booking
+}
+
+func NewMemoryStore() *MemoryStore {
+  return &MemoryStore{
+	 bookings: map[string]Booking{},
+  }
+}
+
+func (s *MemoryStore) Book(b Booking) error
+func (s *MemoryStore) ListBooking(movieID string)  []Booking
