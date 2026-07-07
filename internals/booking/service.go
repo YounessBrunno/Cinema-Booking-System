f62b40c55b89bfe2f963273a91618b1b19950082
youness oukhadjou <brunnotools@gmail.com>
2026-07-07 11:47:59 +0100

    feat(booking): add booking service and concurrent booking test

--- internals/booking/service.go
+++ internals/booking/service.go
@@ -1,1 +1,22 @@
 package booking
+
+
+
+type Service struct {
+	store BookingStore
+}
+
+func NewService(store BookingStore) *Service {
+  return &Service{
+	store: store,
+  }
+}
+
+func (s *Service) Book(booking Booking) error {
+
+   if err := s.store.Book(booking); err != nil {
+	 return err
+   }
+
+   return nil
+}
